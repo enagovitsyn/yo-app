fd51b178d972a768677b4b39c4cdd5ecdc42989d
enagovitsyn <evgeniy.nagovicyn@yandex.ru>
2023-01-15 19:42:46 +0300

    init app

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+import datetime
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    return render_template("index.html")
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    if len(text) > 0:
+        return render_template("yo_page.html", nickname=text)
+    else:
+        return render_template("index.html")
